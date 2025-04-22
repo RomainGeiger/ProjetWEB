a4ebf8ae6a9b11724610ccbd220f9136df769d55
Romain GEIGER <romain.geiger@student.junia.com>
2025-04-22 12:08:50 +0200

    Ajout des avis dans la bdb

--- bdb/projet25_cjm.sql
+++ bdb/projet25_cjm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mar. 22 avr. 2025 à 09:19
+-- Généré le : mar. 22 avr. 2025 à 10:07
 -- Version du serveur : 5.7.24
 -- Version de PHP : 8.3.1
 
@@ -40,7 +40,13 @@ CREATE TABLE `feedbacks` (
 
 INSERT INTO `feedbacks` (`id`, `nom`, `age`, `contenu`) VALUES
 (1, 'Romain', 19, 'Je recommande !'),
-(2, 'Thomas', 19, 'Pas mal mais peut mieux faire');
+(2, 'Thomas', 19, 'Pas mal mais peut mieux faire'),
+(9, 'Georges', 51, 'Votre astuce pour ‘muscler le doigt en changeant de chaîne TV’ m’a changé la vie ! Fini la culpabilité du sport ! Maintenant, je passe des heures à zapper tout en prenant soin de ma forme. Qui a besoin de courir quand on peut télécommander ?'),
+(20, 'Martine', 54, 'Vous m\'avez libérée des \"5 fruits et légumes\" avec votre article \"Les pizzas comptent-elles comme salade composée ?\" Maintenant, je mange mon \"salade tomate oignon\" tous les jours ! Merci pour cette découverte !'),
+(21, 'Tom', 18, 'Depuis que j\'ai adopté la technique du snacking à volonté, c\'est simple : je n\'ai jamais faim ! Pourquoi attendre l\'heure des repas ? Maintenant, j\'ai toujours un paquet de chips à portée de main. Merci pour la liberté !'),
+(22, 'Alex', 29, 'Dormir juste après chaque repas ? Une révolution ! Avant, j’avais de l’insomnie. Maintenant, je dors toute la journée. Qui aurait cru que les siestes post-repas étaient la solution miracle ? Merci, la vie est douce.'),
+(23, 'Fatima', 38, 'Votre guide \"10 excuses pour éviter l’exercice\" est tout simplement brillant ! Avant, je me sentais coupable de ne pas courir. Maintenant, je suis sereine et repose mes chevilles sur le canapé. Vive la relaxation !'),
+(24, 'Olivier', 41, 'Depuis que j’ai remplacé le lait par la crème glacée dans mes céréales, chaque matin est un bonheur. Mon petit-déjeuner a le goût du dessert, et je me sens comme un roi ! Ma balance est peut-être moins fan, mais moi je suis ravi !');
 
 --
 -- Index pour les tables déchargées
@@ -60,7 +66,7 @@ ALTER TABLE `feedbacks`
 -- AUTO_INCREMENT pour la table `feedbacks`
 --
 ALTER TABLE `feedbacks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
