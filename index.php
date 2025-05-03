6741f3783bf8c9dec123b78837353ae19ce77cb1
Tom ENGELS <tom.engels@student.junia.com>
2025-05-04 00:33:15 +0200

    Rajout du goat (Romain) dans l'index

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ else{
             <img src="images/hamburger.jpg" alt="hamburger">
             <h1>Aider à Grossir</h1>
             <div class="resume">
-                <p>Un site créé par MEZDOUR Ghani, ENGELS Tom, FREMION Lucas et ROGER Thomas. Nous sommes honorés de pouvoir vous présenter notre site, il aura pour but d'aider les personnes souhaitant gagner un peu de poid. En leur proposant des produits, des conseils, vous pourrez aussi avoir accès à nos feedback.</p>
+                <p>Un site créé par MEZDOUR Ghani, ENGELS Tom, FREMION Lucas , GEIGER Romain et ROGER Thomas. Nous sommes honorés de pouvoir vous présenter notre site, il aura pour but d'aider les personnes souhaitant gagner un peu de poid. En leur proposant des produits, des conseils, vous pourrez aussi avoir accès à nos feedback.</p>
             </div>
         </article>
     </div>
